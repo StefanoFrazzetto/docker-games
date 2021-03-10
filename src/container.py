333cb3344a462066981e1beb2c51179b152a93d3
Stefano Frazzetto <StefanoFrazzetto@users.noreply.github.com>
2021-03-10 00:26:25 +0000

    Add Docker class to run servers

---
+++ src/container.py
@@ -0,0 +1,22 @@
+import docker
+from docker.errors import DockerException
+
+from servers import Minecraft
+
+
+class Docker:
+
+    @property
+    def client(self) -> docker.DockerClient:
+        try:
+            client = docker.from_env()
+            return client
+        except DockerException as e:
+            raise RuntimeError('Please ensure Docker is running on your system.') from e
+
+    def run(self, server: Minecraft):
+        return self.client.containers.run(
+            server.image_name,
+            detach=True,
+            **server.docker_parameters()
+        )
---
+++ tests/test_container.py
@@ -0,0 +1,49 @@
+from unittest import TestCase
+
+from container import Docker
+from src.servers import Minecraft
+
+
+def suppress_resource_warning():
+    import warnings
+    # filter warning
+    # ResourceWarning: unclosed <socket.socket fd=5, family=AddressFamily.AF_UNIX,
+    # type=SocketKind.SOCK_STREAM, proto=0, raddr=/var/run/docker.sock>
+    warnings.filterwarnings(action="ignore", message="unclosed", category=ResourceWarning)
+
+
+def kill_and_remove_container(container):
+    container.kill()
+    container.remove()
+
+
+def make_minecraft_server():
+    minecraft = Minecraft('mcserver', '1GB', '/tmp/mcserver')
+    minecraft.add_ports(25565, 25565)
+    return minecraft
+
+
+class TestMinecraft(TestCase):
+
+    def test_create_server(self):
+        mc = Minecraft('name', '1GB', '/tmp/mcdata')
+        self.assertIsNotNone(mc)
+
+    def test_run_server(self):
+        suppress_resource_warning()
+        docker = Docker()
+        minecraft = make_minecraft_server()
+        container = docker.run(minecraft)
+        self.assertEqual('created', container.status)
+        kill_and_remove_container(container)
+
+    def test_run_container_raw(self):
+        import docker
+        client = docker.from_env()
+        container = client.containers.run(
+            'itzg/minecraft-server',
+            detach=True,
+            environment={'EULA': 'TRUE'}
+        )
+        self.assertEqual('created', container.status)
+        kill_and_remove_container(container)
