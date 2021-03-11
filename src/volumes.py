f19ccc788f03222d41c9722057f386f5f5027881
Stefano Frazzetto <StefanoFrazzetto@users.noreply.github.com>
2021-03-11 13:59:12 +0000

    Merge Volume into DockerVolume

--- src/volumes.py
+++ src/volumes.py
@@ -1,23 +1,17 @@
 from .validators import Directory, OneOf
 
 
-class Volume(object):
+class DockerVolume:
     source = Directory(allow_not_exist=True)
-
-    def __init__(self, source):
-        self.source = source
-
-    def __repr__(self):
-        return f'{self.__class__.__qualname__}: {self.source}'
-
-
-class DockerVolume(Volume):
     volume_type = OneOf('bind', 'volume')
 
     def __init__(self, source, target, volume_type='bind'):
+        self.source = source
         self.target: str = target
         self.volume_type = volume_type
-        super().__init__(source)
+
+    def __repr__(self):
+        return f'{self.__class__.__qualname__} ({self.volume_type}): {self.source} -> {self.target}'
 
     def dict(self) -> dict:
         return {
