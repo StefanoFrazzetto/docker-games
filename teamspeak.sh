c2e606a2e4969826755935542050cda647e3d934
Stefano Frazzetto <StefanoFrazzetto@users.noreply.github.com>
2021-03-12 17:26:43 +0000

    Remove old scripts

--- teamspeak.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source functions.sh
-
-# Start server
-docker run --name teamspeak \
-       -p 9987:9987/udp -p 10011:10011 -p 30033:30033 \
-       -v "$teamspeak_local_data":/var/ts3server/
-       -e TS3SERVER_LICENSE=accept teamspeak
