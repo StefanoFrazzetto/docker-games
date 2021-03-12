ef136e5ecdd85783753e242090400ea34fefafb8
Stefano Frazzetto <StefanoFrazzetto@users.noreply.github.com>
2021-03-12 17:07:09 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,47 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+    setuptools.setup(
+        name='DockerGames',
+        version='1.0.0-alpha1',
+        author='Stefano Frazzetto',
+        author_email='stefano+pypi@hey.com',
+        description='Easily create game servers using Docker and Python.',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        url='https://github.com/StefanoFrazzetto/AWSomeGamingServer',
+        packages=setuptools.find_packages(),
+        python_requires='>=3.5',
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Operating System :: OS Independent',
+            'Topic :: Games/Entertainment',
+            'Topic :: Communications :: Conferencing',
+            'Topic :: Software Development :: Libraries',
+        ],
+        keywords=[
+            'automation',
+            'game',
+            'multiplayer',
+            'server',
+            'cloud',
+        ],
+        install_requires=[
+            'docker'
+        ],
+        extras_require={
+            'test': ['pytest'],
+        },
+        project_urls={
+            'Bug Reports': 'https://github.com/StefanoFrazzetto/AWSomeGamingServer/issues',
+            'Source': 'https://github.com/StefanoFrazzetto/AWSomeGamingServer',
+        },
+    )
