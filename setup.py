231d878a11e8fe02de5ecc9577d48e97f8001f11
Stefano Frazzetto <StefanoFrazzetto@users.noreply.github.com>
2021-03-14 14:08:45 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
     setuptools.setup(
         name='docker-games',
-        version='1.0.0-alpha3',
+        version='1.0.0-alpha4',
         author='Stefano Frazzetto',
         author_email='stefano+pypi@hey.com',
         description='Easily create game servers using Docker and Python.',
